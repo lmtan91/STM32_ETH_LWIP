8b92ff0bccb87274cc2e43a54783d633328615e5
Tan Minh Le <lmtan91@gmail.com>
2017-05-21 13:45:16 +0700

    disable vPortSVCHandler xPortPendSVHandler xPortSysTickHandler interrupt handlers and define to FreeRTOS API to help FreeRTOS running

--- Apps/httpserver_socket/inc/FreeRTOSConfig.h
+++ Apps/httpserver_socket/inc/FreeRTOSConfig.h
@@ -110,5 +110,10 @@ configKERNEL_INTERRUPT_PRIORITY setting.  Here 15 corresponds to the lowest
 NVIC value of 255. */
 #define configLIBRARY_KERNEL_INTERRUPT_PRIORITY	15
 
+/* define vPortSVCHandler xPortPendSVHandler xPortSysTickHandler to support FreeRTOS */
+#define vPortSVCHandler SVC_Handler
+#define xPortPendSVHandler PendSV_Handler
+#define xPortSysTickHandler SysTick_Handler
+
 #endif /* FREERTOS_CONFIG_H */
 
--- Apps/httpserver_socket/src/stm32f4xx_it.c
+++ Apps/httpserver_socket/src/stm32f4xx_it.c
@@ -113,9 +113,9 @@ void UsageFault_Handler(void)
   * @param  None
   * @retval None
   */
-void SVC_Handler(void)
-{
-}
+//void SVC_Handler(void)
+//{
+//}
 
 /**
   * @brief  This function handles Debug Monitor exception.
@@ -131,19 +131,19 @@ void DebugMon_Handler(void)
   * @param  None
   * @retval None
   */
-void PendSV_Handler(void)
-{
-}
+//void PendSV_Handler(void)
+//{
+//}
 
 /**
   * @brief  This function handles SysTick Handler.
   * @param  None
   * @retval None
   */
-void SysTick_Handler(void)
-{
-  xPortSysTickHandler(); 
-}
+//void SysTick_Handler(void)
+//{
+//  xPortSysTickHandler(); 
+//}
 
 /**
   * @brief  This function handles External line 10 interrupt request.
